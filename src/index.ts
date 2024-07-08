745b1a02c979bfcabb6b17ee9338b06ec332844f
Pamir Perdeci <pamirperdeci@gmail.com>
2024-07-08 13:27:58 -0400

    created Hono server

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,13 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  c.text('GET it')
 })
+
+app.post('/', (c) => {
+  c.text('POST it')
+})
+
 
 const port = 3000
 console.log(`Server is running on port ${port}`)
